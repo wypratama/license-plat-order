29f48d2c097ba74f15fc869a783f0824e1ea4f1c
Wicaksana Pratama <wicaksanapratama@gmail.com>
2023-12-17 23:48:08 +0700

    feat: add server endopoint to provide license data

---
+++ server/api/license.get.ts
@@ -0,0 +1,13 @@
+export default defineEventHandler(() => {
+  return [
+    { nopol: 'KB 9234 KT' },
+    { nopol: 'AA 3245 TYC' },
+    { nopol: 'KB 9133 RE' },
+    { nopol: 'B 9234 TU' },
+    { nopol: 'AD 9124 GH' },
+    { nopol: 'AD 9004 YGU' },
+    { nopol: 'B 9277 IOB' },
+    { nopol: 'AA 1143 BN' },
+    { nopol: 'B 9234 TU' },
+  ]
+})
