bc48aac6dd2235d1206c420912e4376d57ac2b09
Wicaksana Pratama <wicaksanapratama@gmail.com>
2023-12-18 02:27:23 +0700

    feat: Add table component

--- app.vue
+++ app.vue
@@ -1,5 +1,34 @@
+<script setup lang="ts">
+const { data } = useLazyFetch('/api/license', { key: '/api/license' })
+
+const columns = [
+  {
+    header: 'NOPOL',
+    field: 'nopol',
+  },
+  {
+    header: 'WILAYAH',
+    valFunction: (val: { nopol: string }) => getRegion(val.nopol),
+  },
+]
+
+function getRegion(license: string) {
+  if (license.startsWith('KB'))
+    return 'Kalimantan Barat'
+  if (license.startsWith('AA'))
+    return 'Temanggung'
+  if (license.startsWith('B'))
+    return 'Jakarta'
+  if (license.startsWith('AD'))
+    return 'Surakarta'
+  return 'Tidak diketahui'
+}
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <div v-if="data">
+      <LisenceTable :value="data" :columns="columns" />
+    </div>
   </div>
 </template>
---
+++ components/LisenceTable.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts" generic="T">
+type ValFunction<X> = (val: X) => any
+
+const { value, columns } = defineProps<{
+  value: T
+  columns: {
+    header: string
+    field?: string
+    valFunction?: ValFunction
+  }[]
+}>()
+</script>
+
+<template>
+  <ClientOnly>
+    <table>
+      <tr key="header-column">
+        <th v-for="(col) in columns" :key="`${col.header}1190`">
+          {{ col.header }}
+        </th>
+      </tr>
+      <tr v-for="(item, index) in value" :key="index + 125">
+        <td v-for="(col, i) in columns" :key="i + 493849">
+          {{ col.valFunction ? col.valFunction(item) : item[col.field] }}
+        </td>
+      </tr>
+    </table>
+  </ClientOnly>
+</template>
